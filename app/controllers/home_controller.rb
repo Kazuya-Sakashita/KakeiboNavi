f3d4194c4126dd364754d3e48027e517de85ca91
Kazuya-Sakashita <kz0508@gmail.com>
2024-03-24 07:55:31 +0900

    tailwindcss導入

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -1,9 +1,8 @@
 class HomeController < ApplicationController
   def index
     # ダミーデータの生成
-    @line_chart_data = { '2023-01-01' => 5, '2023-01-02' => 3, '2023-01-03' => 6 }
-    @pie_chart_data = { 'Apple' => 10, 'Banana' => 5, 'Grape' => 7 }
-    @bar_chart_data = { '2023-01-01' => 3, '2023-01-02' => 7, '2023-01-03' => 5 }
-    @column_chart_data = { '2023-01-01' => 2, '2023-01-02' => 4, '2023-01-03' => 1 }
+    @line_data = { "2020-01-01": 10, "2020-01-02": 15, "2020-01-03": 20 }
+    @bar_data = { "Apple": 50, "Orange": 70, "Banana": 85 }
+    @pie_data = { "Water": 30, "Fire": 70 }
   end
 end
